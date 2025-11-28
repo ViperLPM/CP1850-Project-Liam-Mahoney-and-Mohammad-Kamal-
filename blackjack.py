5895acb129f0204b45cb213b85e7aca433326602
Mkamal-45 <mohammadaniskamal@gmail.com>
2025-11-27 20:51:34 -0330

    added the write function and tested both the read and write function on the main func

--- blackjack.py
+++ blackjack.py
@@ -9,6 +9,22 @@ def read_money_file():
 
     return current_money
 
+def write_money_file(current_money):
+    with open("money.csv", "w", newline="") as file:
+        writer=csv.writer(file)
+        new_balance= round(current_money, 2)
+        writer.writerow(["Money", new_balance])
+
+
+
+def main():
+    read_money_file()
+    user_input=int(input("Enter amount: "))
+    write_money_file(user_input)
+
+if __name__=="__main__":
+    main()
+
 
 
 
