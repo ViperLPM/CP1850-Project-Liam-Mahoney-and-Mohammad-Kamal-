fcafe545ecb18cfaf1e74b46b1740c2384a90443
Mkamal-45 <mohammadaniskamal@gmail.com>
2025-11-28 19:14:42 -0330

    created a hand_point function where it cacls total hand point and handles the ace. Also point value was changed to int because str is incorrect and program was causing error due to  unsupported operand

--- blackjack.py
+++ blackjack.py
@@ -21,7 +21,21 @@ def draw_card(deck):
     deck.remove(hand)
     return hand
 
+def get_hand_points(hand):
+    points= 0
+    num_aces = 0
 
+    for card in hand:
+        point = card[2]
+        points += point
+
+        if point == 11:
+            num_aces += 1
+
+    while points > 21 and  num_aces >0: #trynna change the ace points from 11-1
+        points -= 10
+        num_aces-= 1
+    return points
 
 
 def main():
@@ -29,13 +43,18 @@ def main():
     print("Blackjack payout is 3:2")
     suit= ["Hearts", "Diamonds", "Clubs", "Spades"]
     rank=["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-    point_value=["2", "3", "4", "5", "6", "7", "8", "9", "10","10","10", "10", "11"]
+    point_value=[2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
     full_deck=create_deck_list(suit,rank,point_value)
-    """checking the amount od cards because getting confused if its drawing correctly or not
+    #checking the amount of cards because getting confused if its drawing correctly or not
     print(f"initial size of deck: {len(full_deck)} ")
     test_draw=draw_card(full_deck)
-    print(test_draw)
-    print(f"New deck size:{len(full_deck)}")"""
+    print(f"Card drawn: {test_draw}")
+    print(f"New deck size:{len(full_deck)}")
+    #testing with a single drawn card
+    single_card_hand= [test_draw]
+    card_points=get_hand_points(single_card_hand)
+    print(f"points for the drawn card is: {card_points}")
+
 
 if __name__=="__main__":
     main()
